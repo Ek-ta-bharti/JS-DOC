70777be99ce5f0db6fdcf9e02139838139c4f823
Ekta bharti <ektabharti235@gmail.com>
2024-04-19 23:38:35 +0530

    19

---
+++ Day/Day5/elseifstatement.js
@@ -0,0 +1,9 @@
+var hour = 14;
+
+if (hour < 12) {
+    console.log("Good morning!");
+} else if (hour < 18) {
+    console.log("Good afternoon!");
+} else {
+    console.log("Good evening!");
+}
---
+++ Day/Day5/elsestatement.js
@@ -0,0 +1,7 @@
+var isRaining = true;
+
+if (isRaining) {
+    console.log("Take an umbrella.");
+} else {
+    console.log("No need for an umbrella.");
+}
