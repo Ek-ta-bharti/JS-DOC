bb498700feb3d0b9668a9d66ba94eadc63050f8b
Ekta bharti <ektabharti235@gmail.com>
2024-04-19 18:18:16 +0530

    19

---
+++ Day/Day2/Day2.js
@@ -0,0 +1,8 @@
+// Using var
+var myVar = 10;
+
+// Using let
+let myLet = 20;
+
+// Using const
+const myConst = 30;
---
+++ Day/Day2/Variable.js
@@ -0,0 +1,22 @@
+let firstNumber =20;
+let secondNumber =80;
+
+const result= firstNumber + secondNumber ;
+
+console.log(result);
+
+
+const resultAddition = firstNumber + secondNumber;
+console.log(resultAddition); 
+
+
+const resultSubtraction = firstNumber - secondNumber;
+console.log(resultSubtraction); 
+
+
+const resultMultiplication = firstNumber * secondNumber;
+console.log(resultMultiplication); 
+
+
+const resultDivision = firstNumber / secondNumber;
+console.log(resultDivision); 
